395431b0f871aaee6fe90622171500c55f81a507
Sassyprogrammer1 <hamzajamshed.cs@gmail.com>
2022-10-01 22:35:17 +0500

    change variable name

--- main.go
+++ main.go
@@ -8,16 +8,20 @@ import (
 )
 
 func main() {
-	c := colly.NewCollector()
-	c.SetRequestTimeout(120 * time.Second)
-	c.OnRequest(func(r *colly.Request) {
+	// coll'ys main entity is a Collector object.
+	// Collector manages the network communication and responsible
+	// for the execution of the attached callbacks while a collector job is running.
+	// To work with colly, you have to initialize a Collector:
+	collector := colly.NewCollector()
+	collector.SetRequestTimeout(120 * time.Second)
+	collector.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
-	c.OnResponse(func(r *colly.Response) {
+	collector.OnResponse(func(r *colly.Response) {
 		fmt.Println("Got a response from", r.Request.URL)
 	})
-	c.OnError(func(r *colly.Response, e error) {
+	collector.OnError(func(r *colly.Response, e error) {
 		fmt.Println("Got this error:", e)
 	})
-	c.Visit("https://jumia.com.ng/")
+	collector.Visit("https://www.linkedin.com/jobs/search/?currentJobId=3227124654&geoId=101282230&keywords=backend%20engineer&location=Germany&refresh=true")
 }
